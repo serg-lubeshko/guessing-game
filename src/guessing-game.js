412d241da09f304fb6801a47188884bd89b9571a
Sergei_L <lubeshkosergei@gmail.com>
2023-02-26 23:58:26 +0300

    feat:: add solve

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.max - this.min) / 2) + this.min;
     }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
 
